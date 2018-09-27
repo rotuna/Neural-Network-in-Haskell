d0950c37bd2aa0f6a57090bcc8de6d5e1221dc6a
rotuna <sadhanasrinivasan@protonmail.com>
2018-09-27 08:47:34 +0530

    Added softmax and check for list length in dot

--- functions.hs
+++ functions.hs
@@ -1,14 +1,27 @@
 module Functions where
 
+-- Constants
+e::(Floating a) => a
+e = 2.71828
+
 dot ::(Floating a) => [a] -> [a] -> a
--- Should probably check both lists are of same length
-dot x y = sum [a*b | (a,b) <- zip x y]
+-- Function to to get dot product of two lists
+-- Throws an error is the lists are not equal length
+dot x y 
+        | length x == length y = sum [a*b | (a,b) <- zip x y]
+        | otherwise            = error "Invalid Inputs: Lists of unequal length"
 
 sigmoid ::(Floating a) => [a] -> [a] -> a
-sigmoid inputs weights = 1/(1 + (2.71828)**(-1* ( dot inputs weights )))
+sigmoid inputs weights = 1/(1 + (e)**(-1* ( dot inputs weights )))
 
 relu ::(Floating a, Ord a) => [a] -> [a] -> a
 relu inputs weights  
         | value > 0.0 = value
         | otherwise   = 0
         where value = dot inputs weights
+
+softmax ::(Floating a) => [a] -> [a]
+softmax logits = 
+        let exponents = [(e)**x | x <- logits]
+            exponents_sum = sum exponents
+        in [exponent/exponents_sum | exponent <- exponents]
