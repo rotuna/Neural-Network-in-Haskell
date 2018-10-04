2b488b0741ce1edd60e7f8fdb7c55900edba2dbe
rotuna <sadhanasrinivasan@protonmail.com>
2018-10-04 13:49:06 +0530

    Added grad for relu and sigmoid

--- functions.hs
+++ functions.hs
@@ -11,13 +11,16 @@ dot x y
         | length x == length y = sum [a*b | (a,b) <- zip x y]
         | otherwise            = error "Invalid Inputs: Lists of unequal length"
 
-sigmoid ::(Floating a) => [a] -> [a] -> a
-sigmoid inputs weights = 1/(1 + (e)**(-1* ( dot inputs weights )))
+sigmoid ::(Floating a) => [a] -> [a] -> Bool -> a
+sigmoid inputs weights grad 
+        | grad = e**x + e**((-2)*x)
+        | otherwise = 1/(1 + (e)**(-1* ( x )))
+        where x = dot inputs weights
 
-relu ::(Floating a, Ord a) => [a] -> [a] -> a
-relu inputs weights  
-        | value > 0.0 = value
-        | otherwise   = 0
+relu ::(Floating a, Ord a) => [a] -> [a] -> Bool -> a
+relu inputs weights grad
+        | value > 0.0 = if grad then 1.0 else value
+        | otherwise   = 0.0
         where value = dot inputs weights
 
 softmax ::(Floating a) => [a] -> [a]
